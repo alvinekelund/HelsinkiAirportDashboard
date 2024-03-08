249583ebf6f50a235a636d57f279af5e69bec8a7
Alvin Ekelund <alvin.ekelund@aalto.fi>
2024-03-08 11:40:34 +0200

    UI design thought out and  implementation started.

---
+++ .metals/.reports/bloop/2024-03-07/r_WARNING: Adding Menu..._21-21-35-291.md
@@ -0,0 +1,7 @@
+error id: 2y+NaxLgFZqo+h3+XBCTdA==
+### Bloop error:
+
+WARNING: Adding MenuItem Exit that has already been added to File
+#### Short summary: 
+
+WARNING: Adding MenuItem Exit that has already been added to File
--- src/main/scala/Dashboard.scala
+++ src/main/scala/Dashboard.scala
@@ -7,6 +7,10 @@ import finaviaAPI.APIClient.*
 import scalafx.scene.control._
 import scalafx.Includes._
 import scalafx.event.ActionEvent
+import javafx.scene.layout.VBox
+import java.awt.Graphics
+import scalafx.stage.StageStyle.Unified
+import javafx.stage.StageStyle
 
 
 object Dashboard extends JFXApp3:
@@ -14,15 +18,43 @@ object Dashboard extends JFXApp3:
   def start() =
 
     stage = new JFXApp3.PrimaryStage:
-      title = "Helsinki Airport Dashboar"
+      title = "Helsinki Airport Dashboard"
       width = 1500
       height = 900
+      initStyle(StageStyle.DECORATED)
 
     val root = Pane()
-
     val scene = Scene(parent = root)
     stage.scene = scene
 
+
+    // val vbox = new VBox(10, new Button("Home"), new Button("Settings"))
+
+
+
+    val menuBar = new MenuBar
+    val fileMenu = new Menu("File")
+    val settingsMenu = new Menu("Settings")
+    val exitItem = new MenuItem("Exit")
+    val colorItem = new MenuItem("Color")
+    fileMenu.items = List(exitItem)
+    settingsMenu.items = List(colorItem)
+    menuBar.menus = List(fileMenu, settingsMenu)
+    menuBar.prefWidth = 1500
+
+    
+    val tabPane = new TabPane
+    val homeTab = new Tab
+    homeTab.text = "Home"
+    val dataTab = new Tab
+    dataTab.text = "Data"
+    tabPane.layoutY = 30
+    tabPane.minWidth = 1500
+    tabPane.tabs = List(homeTab, dataTab)
+
+
+
+    /*
     val button = new Button("Click me")
     button.layoutX = 100
     button.layoutY = 100
@@ -36,7 +68,7 @@ object Dashboard extends JFXApp3:
     listView.layoutY = 150
   
 
-    root.children += (button, comboBox, listView)
+    
 
     button.onAction = (e: ActionEvent) => {
       val selected = listView.selectionModel.apply().getSelectedItems
@@ -47,8 +79,10 @@ object Dashboard extends JFXApp3:
     comboBox.onAction = (e: ActionEvent) => {
       listView.items.apply() += comboBox.selectionModel.apply().getSelectedItem
     }
-    
-    // getFlightData()
+    */
+    //getFlightData()
+
+    root.children += (menuBar, tabPane)
 
   end start
 
